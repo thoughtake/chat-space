766c3daef5171755190be3923bc80a52f498b1ed
thoughtake <sought1001@gmail.com>
2020-02-12 09:14:54 +0900

    remove respond  format.html from messController

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,7 +10,7 @@ class MessagesController < ApplicationController
     @message = @group.messages.new(message_params) 
     if @message.save
       respond_to do |format|
-        format.html { render redirect_to group_messages_path(@group.id), notice: "メッセージが送信されました" }
+        # format.html { render redirect_to group_messages_path(@group.id), notice: "メッセージが送信されました" }
         format.json
       end
     else
