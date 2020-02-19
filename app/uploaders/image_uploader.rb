93d97a815dd50c76a9d0ebcd77e8a9d2b867d18e
thoughtake <sought1001@gmail.com>
2020-02-19 20:43:41 +0900

    resize_to_fit 300x300

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -12,6 +12,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  process resize_to_fit: [300, 300]
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
@@ -28,9 +29,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process resize_to_fit: [800, 800]
-  end
+  # version :thumb do
+  #   process resize_to_fit: [300, 300]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
