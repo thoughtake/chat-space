15b30be0ce6ea52caec61e69c8483622e4a8d604
thoughtake <sought1001@gmail.com>
2020-02-19 11:38:10 +0900

    Update image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -28,9 +28,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  process resize_to_fit: [800, 800]
-  # end
+  version :thumb do
+    process resize_to_fit: [800, 800]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
